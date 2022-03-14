23ababa1adc11c84188fa142194165510fce56a1
Dvir Borochov <dvir.borochov@gmail.com>
2022-03-14 08:48:26 +0200

    add main mf

---
+++ mainMF.mk
@@ -0,0 +1,26 @@
+#!make -f
+
+CXX=clang++-9 
+CXXFLAGS=-std=c++2a -Werror
+
+HEADERS=mat.hpp
+OBJECTS=mat.o
+SOURCES=mat.cpp
+
+run: main 
+	./$^
+
+main: main.o $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $^ -o main
+
+test: TestCounter.o Test.o $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $^ -o test
+
+tidy:
+	clang-tidy $(SOURCES) -checks=bugprone-*,clang-analyzer-*,cppcoreguidelines-*,performance-*,portability-*,readability-* --warnings-as-errors=-* --
+
+%.o: %.cpp $(HEADERS)
+	$(CXX) $(CXXFLAGS) --compile $< -o $@
+
+clean:
+	rm -f *.o demo test
